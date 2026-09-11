b5818d7dd0e2ed257ea7ef465e4f9f3bd2b3493c
viniciusreis15-cpu <vinicius.reis15@escola.pr.gov.br>
2026-09-11 11:18:42 -0300

    Add async function to fetch random dog image

---
+++ app.js
@@ -0,0 +1,10 @@
+// async ==> assicrono
+async function imagemApi(){
+// esperar a resposta  ==> await
+let respostaApi = await fetch('https://dog.ceo/api/breeds/image/random');
+let dadosApi = await respostaApi.json();
+document.querySelector('.img-dog').src = dadosApi.message;
+console.log(dadosApi);
+
+}
+imagemApi()
